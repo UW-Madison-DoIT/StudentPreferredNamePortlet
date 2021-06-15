b4449cf4f9795378db6358b0a7de8051e85764ef
Andrew Petro <andrew.petro@wisc.edu>
2021-06-15 10:59:44 -0500

    bootstrap Latin-9 last name similarity validation

--- student-preferred-name-portlet-api/src/main/java/edu/wisc/portlet/preferred/form/validator/Latin9LastNameSimilarValidator.java
+++ student-preferred-name-portlet-api/src/main/java/edu/wisc/portlet/preferred/form/validator/Latin9LastNameSimilarValidator.java
@@ -6,6 +6,10 @@ import org.springframework.validation.Validator;
 
 /**
  * Validator requiring that preferred last name be similar to legal last name, considering the LATIN-9 character set.
+ *
+ * When the preferred lastName is not similar to the legal last name, sets validation failure
+ *
+ * lastName : error.notSimilarToLegalName
  */
 public class Latin9LastNameSimilarValidator  implements Validator  {
   @Override
@@ -15,6 +19,15 @@ public class Latin9LastNameSimilarValidator  implements Validator  {
 
   @Override
   public void validate(Object target, Errors errors) {
+
+    PreferredNameExtended pne = (PreferredNameExtended) target;
+
+    String preferredLastName = pne.getLastName();
+    String legalLastName = pne.getLegalLastName();
+
+    if ((preferredLastName != null) && (! preferredLastName.equals(legalLastName))) {
+      errors.rejectValue("lastName", "error.notSimilarToLegalName");
+    }
 
   }
 }
--- student-preferred-name-portlet-api/src/test/java/edu/wisc/portlet/preferred/form/validator/Latin9LastNameSimilarValidatorTest.java
+++ student-preferred-name-portlet-api/src/test/java/edu/wisc/portlet/preferred/form/validator/Latin9LastNameSimilarValidatorTest.java
@@ -3,7 +3,11 @@ package edu.wisc.portlet.preferred.form.validator;
 import edu.wisc.portlet.preferred.form.PreferredName;
 import edu.wisc.portlet.preferred.form.PreferredNameExtended;
 import org.junit.Test;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.MapBindingResult;
 import org.springframework.validation.Validator;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +25,40 @@ public class Latin9LastNameSimilarValidatorTest {
     assertFalse(validator.supports(PreferredName.class));
   }
 
+  /**
+   * Test that a preferred last name identical to the legal last name is valid.
+   */
+  @Test
+  public void matchingLastNameIsValid() {
 
+    PreferredNameExtended pne = new PreferredNameExtended();
+    pne.setLastName("Badger");
+    pne.setLegalLastName("Badger");
+
+    BindingResult br = new MapBindingResult(new HashMap<String, String>(), "pn");
+
+    validator.validate(pne, br);
+
+    assertFalse (br.hasErrors());
+  }
+
+  /**
+   * Test that people who do not have last name Blank can't just decide to have last name Blank.
+   */
+  @Test
+  public void peopleNotNamedBlankCannotImpersonateTheChancellor(){
+
+    PreferredNameExtended pne = new PreferredNameExtended();
+    pne.setLastName("Blank"); // prefer to have the Chancellor's last name
+    pne.setLegalLastName("Smith"); // but legally has last name Smith
+
+    BindingResult br = new MapBindingResult(new HashMap<String, String>(), "pn");
+
+    validator.validate(pne, br);
+
+    assertTrue("Should fail validation because preferred last name is wildly dissimilar to legal last name.",
+      ValidatorTestSupport.fieldHasError("lastName", "error.notSimilarToLegalName", br));
+
+  }
 
 }
